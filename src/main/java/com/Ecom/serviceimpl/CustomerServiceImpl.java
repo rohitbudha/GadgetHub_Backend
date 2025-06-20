ece741395edb1d4b6a0908a5bcc1fb7d9fa8e81e
Laxman-Nath <nathlaxman242@gmail.com>
2025-06-20 07:38:43 +0545

    fix : fixed pom.xml file

--- src/main/java/com/Ecom/serviceimpl/CustomerServiceImpl.java
+++ src/main/java/com/Ecom/serviceimpl/CustomerServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.Ecom.models.Customer;
 import com.Ecom.repos.CustomerRepo;
 import com.Ecom.services.CustomerService;
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
 
 	@Autowired
